e78b4b39eb51777c518b71853a85e969eaa71cec
Drew Altschul <dmaltschul@gmail.com>
2018-08-03 19:07:04 +0100

    Post-skype phylogenetics commit

--- analysis/07_phylo-t_proto.R
+++ analysis/07_phylo-t_proto.R
@@ -19,15 +19,31 @@ mp.tree[[1]]$tip.label
 plotTree(mp.tree[[1]], type='cladogram')
 
 
+
 ### Preparing test objects
 
 phylo.df = aggregate(mp_data$correct, by=list(mp_data$species,mp_data$condition),
                      FUN=mean)
 
+phylo.df.all = aggregate(mp_data$correct, by=list(mp_data$species),
+                     FUN=mean)
+
 phylo.df$sd = aggregate(mp_data$correct, by=list(mp_data$species,mp_data$condition),
                       FUN=sd)[[3]]
 
-colnames(phylo.df) = c('species','condition','mean','sd')
+phylo.df.all$sd = aggregate(mp_data$correct, by=list(mp_data$species),
+                        FUN=sd)[[2]]
+
+
+colnames(phylo.df) = c('species','condition',
+                       'mean','sd')
+
+colnames(phylo.df.all) = c('species',
+                       'mean','sd')
+
+
+phylo.df = phylo.df.all
+
 
 phylo.df %>%  
   filter(species != 'black_faced_spider_monkey') -> phylo.df
@@ -91,20 +107,29 @@ phylo.table.long = phylo.table.short
 phylo.table.long[,'Ha'] = phylo.df$mean[phylo.df$condition=='long']
 phylo.table.long[,'Ha.SE'] = phylo.df$sd[phylo.df$condition=='long']
 
+phylo.table.all = data.frame(Ha=numeric(11),H0=rep.int(1/3,11),Ha.SE=numeric(11),H0.SE=rep.int(0,11))
+row.names(phylo.table.all) = phylo.df%>%pull(species)
+phylo.table.all$Ha = phylo.df$mean
+phylo.table.all$Ha.SE = phylo.df$sd
+phylo.table.all = as.matrix(phylo.table.all)
+
 
 
 ### Estimate lambda starting value in advance
 
-l.short = phylosig(mp.tree[[1]],x=phylo.table.short[,'Ha'],
+l.short = phylosig(mp.tree[[1]],x=phylo.table.short[,'Ha'],test=TRUE,
          method='lambda')
 
-l.medium = phylosig(mp.tree[[1]],x=phylo.table.medium[,'Ha'],
+l.medium = phylosig(mp.tree[[1]],x=phylo.table.medium[,'Ha'],test=TRUE,
                    method='lambda')
 
-l.long = phylosig(mp.tree[[1]],x=phylo.table.long[,'Ha'],
+l.long = phylosig(mp.tree[[1]],x=phylo.table.long[,'Ha'],test=TRUE,
                    method='lambda')
 
-
+l.all = phylosig(mp.tree[[1]],x=phylo.table.all[,'Ha'],test=TRUE,
+                 method='lambda')
+  
+  
 
 ### T-testing
 
@@ -114,6 +139,8 @@ short.t = phyl.pairedttest(mp.tree[[1]],
                            lambda = l.short$lambda
                            )
 short.t
+
+
 
 medium.t = phyl.pairedttest(mp.tree[[1]],
                            x1=phylo.table.medium[,'Ha'],x2=phylo.table.medium[,'H0'],
@@ -133,12 +160,31 @@ long.t
 
 
 
+all.t = phyl.pairedttest(mp.tree[[1]],
+                          x1=phylo.table.all[,'Ha'],x2=phylo.table.all[,'H0'],
+                          se1=phylo.table.all[,'Ha.SE'],se2=phylo.table.all[,'H0.SE'],
+                          lambda = l.all$lambda
+)
+all.t
+
+
+
+# all.t = phyl.pairedttest(mp.tree[[1]],
+#                           x1=phylo.table.all[,'Ha'],x2=phylo.table.all[,'H0'],
+#                           se1=phylo.table.all[,'Ha.SE'],se2=phylo.table.all[,'H0.SE'],
+#                           lambda = l.all$lambda
+# )
+# all.t
+
+
+
 ### Possible LR tests
 
 ## Trying to reproduce _ et al. (2010)
 # LR.D = 2.579 - 2*0 # second part of this is the question
 # Ddf = 1
 # 1 - pchisq(LR.D, Ddf, ncp=0, lower.tail=TRUE, log.p=FALSE) ## this matches Table 2, line 2
+
 
 
 LR.D = abs(2*short.t$logL)
@@ -156,3 +202,25 @@ Ddf = 1
 LR.D = abs(2*long.t$logL)
 Ddf = 1
 1 - pchisq(LR.D, Ddf, ncp=0, lower.tail=TRUE, log.p=FALSE)
+
+
+
+LR.D = abs(2*all.t$logL)
+Ddf = 1
+1 - pchisq(LR.D, Ddf, ncp=0, lower.tail=TRUE, log.p=FALSE)
+
+
+
+### Ancestral state reconstructions
+
+phylo.table.all = phylo.table.all[,'Ha']
+phylo.table.all = phylo.table.all - 1/3
+
+priMemAnc = fastAnc(mp.tree[[1]], phylo.table.all, CI = TRUE)
+
+obj<-contMap(anole.tree,svl,plot=FALSE)
+plot(obj,type="fan",legend=0.7*max(nodeHeights(anole.tree)),
+     fsize=c(0.7,0.9))
+
+
+## lambda for overall t.test?
